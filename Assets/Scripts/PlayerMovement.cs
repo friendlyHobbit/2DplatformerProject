1df7350fbacfc1ba71bddb0f143c8f56de3e98a3
friendlyHobbit <sitavriend@gmail.com>
2020-06-17 16:24:42 +0200

    prefabs and script
    playermovement script, prefabs of platform, enemy and player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+
+
+    }
+
+
+    void playerMovement()
+    {
+        // player presses left arrow
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + new Vector3(-Time.deltaTime * 5, 0, 0);
+        }
+        // player presses right arrow
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(Time.deltaTime * 5, 0, 0);
+        }
+    }
+
+
+
+}
