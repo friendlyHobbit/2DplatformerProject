710a735fc34a0f8db51946de88f371e440d1e1d5
friendlyHobbit <sitavriend@gmail.com>
2020-06-17 17:11:15 +0200

    game manager
    added GameManager with start and exit buttons

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //buttons
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        print("exit game");
+        Application.Quit();
+    }
+}
